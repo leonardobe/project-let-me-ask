627bef71ec21219462692eec2f2e2083978e0e45
Leonardo Curtis <75338383+leonardobe@users.noreply.github.com>
2025-07-14 11:05:33 -0300

    feat(web): setup UI and implement rooms flow

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreatRoom } from './pages/create-rooms'
+import { RecordRoomAudio } from './pages/record-room-audio'
 import { Room } from './pages/rooms'
 
 const queryClient = new QueryClient()
@@ -12,6 +13,7 @@ export function App() {
         <Routes>
           <Route element={<CreatRoom />} index />
           <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
